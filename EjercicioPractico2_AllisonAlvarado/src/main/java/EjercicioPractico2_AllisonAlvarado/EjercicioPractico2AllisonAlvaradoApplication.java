1be933b406e60664fc9560762e7f3657daece32f
alli00152 <allison.alvarado2007@gmail.com>
2026-04-15 18:09:01 -0600

    Creación del Proyecto

---
+++ EjercicioPractico2_AllisonAlvarado/src/main/java/EjercicioPractico2_AllisonAlvarado/EjercicioPractico2AllisonAlvaradoApplication.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2_AllisonAlvarado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPractico2AllisonAlvaradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPractico2AllisonAlvaradoApplication.class, args);
+	}
+
+}
---
+++ EjercicioPractico2_AllisonAlvarado/src/test/java/EjercicioPractico2_AllisonAlvarado/EjercicioPractico2AllisonAlvaradoApplicationTests.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2_AllisonAlvarado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPractico2AllisonAlvaradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
